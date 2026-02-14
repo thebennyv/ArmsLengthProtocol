45f180288c75bd1f081e953cb35268971ec41a1f
Ben <benviso@yahoo.com>
2026-02-14 15:01:50 -0500

    Updated description

--- ArmsLengthProtocol.ino
+++ ArmsLengthProtocol.ino
@@ -3,17 +3,17 @@
 //
 // Description:
 //   An Iron Man suit controller built around Arduino, Bluetooth Low Energy,
-//   and the Xiao ESP32 C6 microncontroller.
+//   and the Xiao ESP32 C6 microcontroller.
 //   This project is inspired by https://github.com/crashworks3d/Iron_Man_Servo,
 //   but uses none of its code.
 //
 // Dependencies:
 //   Arduino Libraries:
-//     OneButton by Matthias Hertel 2.6.1
-//     Adafruit Neopixel 1.15.2
-//     FastLED by Daniel Garcia 3.10.3
-//     ESP32PWM
-//     ESP32Servo
+//     OneButton by Matthias Hertel v2.6.1
+//     ESP32Servo by Kevin Harrington, John K. Bennett v3.1.3
+//       (with ESP32PWM)
+//     Adafruit Neopixel v1.15.2
+//     FastLED by Daniel Garcia v3.10.3
 // =============================================================================
 
 #include <Arduino.h>
